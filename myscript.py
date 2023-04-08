940c39104153b499a25936dafa071dd6c61ca8e6
karl <ksmullercha@gmail.com>
2023-04-08 15:44:29 -0400

    test

--- myscript.py
+++ myscript.py
@@ -1,12 +1,13 @@
 import os
 import subprocess
 import sys
+import uuid
 
 # Get the input argument from the command line
 input_str = sys.argv[1]
 
-# Define the name of the new branch
-new_branch_name = "new-feature-branch"
+# Define the name of the new branch as a UUID
+new_branch_name = str(uuid.uuid4())
 
 # Checkout a new branch
 subprocess.run(["git", "checkout", "-b", new_branch_name])
@@ -22,12 +23,4 @@ subprocess.run(["git", "add", new_file_path])
 # Commit the changes to the new branch
 subprocess.run(["git", "commit", "-m", "Added new file to branch"])
 
-# Push the changes to the remote repository
-subprocess.run(["git", "push", "-u", "origin", new_branch_name])
-
-# Create a pull request for the new branch
-# Replace <pull_request_title> with the desired title for the pull request
-pull_request_title = "Added new file to branch"
-subprocess.run(["gh", "pr", "create", "--title", pull_request_title, "--body", "Please review and merge this pull request", "--head", new_branch_name, "--base", "main"])
-
-sys.stdout.write('done')
+sys.stdout.write(new_branch_name)
